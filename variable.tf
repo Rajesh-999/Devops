46eeb070de46680c2711cce44999070bc87deecb
Rajesh-999 <rajesh.krishna32@gmail.com>
2024-08-28 11:05:08 +0530

    Update variable.tf
    variable ami_ids added

--- variable.tf
+++ variable.tf
@@ -2,3 +2,13 @@ variable "region" {
      description = "The AWS region to create the instance in"
      type        = string
 }
+
+variable "ami_ids" {
+  description = "A map of AMI IDs by region"
+  type        = string
+ # default = {
+ #  "us-west-1" = "ami-066784287e358dad1"
+ #  "us-east-1" = "ami-0abcdef1234567890"
+ #  "eu-central-1" = "ami-0abcd1234efgh5678"
+ #}
+}
